39500f3e0c0099b651437119195dcf1112c3a3c2
Adam Nettel <netteladam@gmail.com>
2019-09-03 21:20:55 -0500

    officers

--- bot.js
+++ bot.js
@@ -418,10 +418,7 @@ function mention(ids, name) {
 function namelessMention(ids) {
   var attachments = {
     "type":"mentions",
-    "user_ids":ids,
-    "loci":[
-      [0, 1]
-    ]
+    "user_ids":ids
   };
 
   return attachments;
